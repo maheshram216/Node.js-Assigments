4045e1876f5c5f37f4fb11edc85d74798d95c0fa
mahesh <maheshram216@gmail.com>
2022-09-29 22:19:37 +0530

    Ass-5 added

---
+++ node.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const http = require('http');
+
+fs.writeFile('index.html','<h1> Hello World </h1>\n <p> This is Mahesh... </p>',(err)=>{
+    console.log(err);
+});
+
+const server = http.createServer((req,res)=>{
+    fs.readFile('index.html','utf-8',(err,data)=>{
+        res.writeHead(200, {"content-type": "text/html"});
+        res.write(data);
+        res.end();
+    });
+})
+
+server.listen(5000, ()=>console.log("server started..."));
